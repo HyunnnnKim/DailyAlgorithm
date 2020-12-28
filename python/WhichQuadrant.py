8d21c682665888cd0420e4d628c94083bafe3256
hyunnnnkim <hwkim931224@gmail.com>
2020-12-28 09:42:13 +0900

    getting started

---
+++ python/WhichQuadrant.py
@@ -0,0 +1,11 @@
+# Number 14681
+
+def whichQuadrant():
+    if pointX > 0:
+        print(1) if pointY > 0 else print(4)
+    elif pointX < 0:
+        print(2) if pointY > 0 else print(3)
+
+pointX = float(input())
+pointY = float(input())
+whichQuadrant()
