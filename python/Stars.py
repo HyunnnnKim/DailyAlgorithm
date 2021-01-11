ca8a2604830e96b6a9e04658046845c4d6615f69
hyunnnnkim <hwkim931224@gmail.com>
2021-01-11 21:03:51 +0900

    more stars

--- python/Stars.py
+++ python/Stars.py
@@ -15,5 +15,34 @@ def stars2(num):
             print('*', end='')
         print()
 
-stars1(int(input()))
-stars2(int(input()))
+# Number 2440
+def stars3(num):
+    for i in range(num):
+        for _ in range(i, num):
+            print('*', end='')
+        print()
+
+# Number 2441
+def stars4(num):
+    for i in range(num):
+        for _ in range(num - i, num):
+            print(' ', end='')
+        for _ in range(num - i, 0, -1):
+            print('*', end='')
+        print()
+
+# Number 2442
+def stars5(num):
+    for i in range(num):
+        for _ in range(i, num - 1):
+            print(' ', end='')
+        for _ in range(0, (i * 2) + 1):
+            print('*', end='')
+        print()
+
+n = int(input())
+stars1(n)
+stars2(n)
+stars3(n)
+stars4(n)
+stars5(n)
